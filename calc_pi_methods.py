775a38df0e4b2157d5ddf31b0ebcac2fec0e4728
vkorotkikh <va.korotki@gmail.com>
2017-08-05 21:43:58 -0400

    Additions

---
+++ calc_pi_methods.py
@@ -0,0 +1,23 @@
+# ******************************************************************************
+# Name:    Pi calculations
+# Author:  Vadim Korotkikh
+# Description: Calcutate Pi
+#
+# ******************************************************************************
+# Begin Imports
+
+import math
+# from gmpy2 import mpz
+from time import time
+
+def main():
+	print("test")
+
+
+def some_func():
+	pass
+
+
+
+if __name__ == "__main__":
+	main()
